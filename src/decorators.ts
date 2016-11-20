ede1e70129b9196b5889dabd42df2701aed46c8b
Charly POLY <cpoly55@gmail.com>
2016-11-20 22:18:22 +0100

    remove useless code

--- src/decorators.ts
+++ src/decorators.ts
@@ -2,11 +2,6 @@ export default (client) => {
    return {
       SpotifyQuery(query: string) {
          return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-            // save a reference to the original method this way we keep the values currently in the
-            // descriptor and don't overwrite what another decorator might have done to the descriptor.
-            if(descriptor === undefined) {
-               descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
-            }
             var originalMethod = descriptor.value;
 
             descriptor.value = function (variables?) {
