ebef8546f54fd585c81034c5cc97279bdf8b8b17
Charly POLY <cpoly55@gmail.com>
2016-11-24 17:13:21 +0100

    some specs

---
+++ spec/decorators/spotify-query.spec.ts
@@ -0,0 +1,37 @@
+import SpotifyDecorators from '../../src/decorators';
+
+describe('SpotifyQuery Decorator', () => {
+
+  describe('when data fetching is a success', () =>  {
+
+    it('should call method', (done: Function) => {
+      let data = [{track: 'Track'}];
+
+      let fakeSpotifyClient = {
+        query: (): Promise<any> => Promise.resolve({ data: data, errors: null })
+      }
+
+      let myMock = jasmine.createSpy('spy');
+
+      let { SpotifyQuery } = SpotifyDecorators(fakeSpotifyClient);
+
+      class A {
+
+        @SpotifyQuery(`
+          track(id: 'id') {
+            name
+          }
+        `)
+        method(data?): any {
+          console.log('method');
+          myMock(data);
+        }
+      }
+
+      (new A).method().then(() => {
+        expect(myMock).toHaveBeenCalledWith(data);
+        done();
+      }, done);
+    })
+  });
+});
