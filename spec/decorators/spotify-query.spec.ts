2e3eb036718cf9f36935540ac2622e2faa6cf38c
Charly POLY <cpoly55@gmail.com>
2016-11-24 23:49:48 +0100

    basic specs

--- spec/decorators/spotify-query.spec.ts
+++ spec/decorators/spotify-query.spec.ts
@@ -2,16 +2,20 @@ import SpotifyDecorators from '../../src/decorators';
 
 describe('SpotifyQuery Decorator', () => {
 
+  function buildFakeSpotifyClient (resolver: Function) {
+    return  {
+        query: resolver
+      }
+  }
+
   describe('when data fetching is a success', () =>  {
 
     it('should call method', (done: Function) => {
       let data = [{track: 'Track'}];
 
-      let fakeSpotifyClient = {
-        query: (): Promise<any> => Promise.resolve({ data: data, errors: null })
-      }
+      let fakeSpotifyClient = buildFakeSpotifyClient(() => Promise.resolve({ data: data, errors: null }))
 
-      let myMock = jasmine.createSpy('spy');
+      let successSpy = jasmine.createSpy('success');
 
       let { SpotifyQuery } = SpotifyDecorators(fakeSpotifyClient);
 
@@ -23,14 +27,46 @@ describe('SpotifyQuery Decorator', () => {
           }
         `)
         method(data?): any {
-          myMock(data);
+          successSpy(data);
         }
       }
 
       (new A).method().then(() => {
-        expect(myMock).toHaveBeenCalledWith(data);
+        expect(successSpy).toHaveBeenCalledWith(data);
         done();
       }, done);
+    })
+  });
+
+  describe('when data fetching is a failure', () =>  {
+
+    it('should not call method', (done: Function) => {
+      let data = [{track: 'Track'}];
+
+      let fakeSpotifyClient = buildFakeSpotifyClient(() => Promise.reject('error'))
+
+      let successSpy = jasmine.createSpy('success');
+      let failureSpy = jasmine.createSpy('failure');
+
+      let { SpotifyQuery } = SpotifyDecorators(fakeSpotifyClient);
+
+      class A {
+
+        @SpotifyQuery(`
+          track(id: 'id') {
+            name
+          }
+        `)
+        method(data?): any {
+          successSpy(data);
+        }
+      }
+
+      (new A).method().then(() => {}, failureSpy).then(() => {
+        expect(successSpy).not.toHaveBeenCalled();
+        expect(failureSpy).toHaveBeenCalledWith('error');
+        done();
+      });
     })
   });
 });
