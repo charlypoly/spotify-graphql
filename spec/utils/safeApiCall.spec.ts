ebe20fff644ad9a3346ba9f1080366c416ac3f28
Charly POLY <wittydeveloper@users.noreply.github.com>
2016-12-07 10:11:46 +0100

    spec(utils/safeApiCall): add specs for safeApiCall (#21)

---
+++ spec/utils/safeApiCall.spec.ts
@@ -0,0 +1,97 @@
+import { safeApiCall } from '../../lib/utils';
+import { spotifyWebAPIClient } from '../../lib/client';
+import * as nock from 'nock';
+
+describe('safeApiCall', () => {
+
+  nock.disableNetConnect();
+
+  let client = spotifyWebAPIClient({
+    "clientId": "clientId",
+    "clientSecret": "clientSecret",
+    "redirectUri": "http://redirectUri.dev",
+    "accessToken": "accessToken"
+  });
+
+
+  describe('when response is 200', () => {
+    let request;
+    beforeEach(() => {
+      request = nock('https://api.spotify.com:443')
+        .get('/v1/me')
+        .reply(200, {});
+    });
+    afterEach(() =>  {
+      nock.cleanAll();
+    });
+
+    it('should call promise success callback', (done) => {
+      let onSuccess = function () {
+        expect(request.isDone()).toBeTruthy();
+        done();
+      }
+
+      let onError = () => { throw 'should not be called' };
+
+      safeApiCall(
+        client,
+        'getMe'
+       ).then(onSuccess).catch(onError);
+    });
+  });
+
+  describe('when response is an error', () => {
+    let request;
+    beforeEach(() => {
+      request = nock('https://api.spotify.com:443')
+        .get('/v1/me')
+        .reply(500, {});
+    });
+    afterEach(() =>  {
+      nock.cleanAll();
+    });
+
+    it('should call promise error callback', (done) => {
+      let onError = function () {
+        expect(request.isDone()).toBeTruthy();
+        done();
+      }
+
+      let onSuccess = () => { throw 'should not be called' };
+
+      safeApiCall(
+        client,
+        'getMe'
+       ).then(onSuccess).catch(onError);
+    });
+  });
+
+  describe('when formatter is provided', () => {
+    let request;
+    beforeEach(() => {
+      request = nock('https://api.spotify.com:443')
+        .get('/v1/me')
+        .reply(200, { results: [1, 2, 3] });
+    });
+    afterEach(() =>  {
+      nock.cleanAll();
+    });
+
+    it('should apply given formatter on results', (done) => {
+      let onSuccess = function (results) {
+        expect(results).toEqual([1, 2, 3]);
+        expect(request.isDone()).toBeTruthy();
+        done();
+      }
+
+      let onError = () => { throw 'should not be called' };
+
+      safeApiCall(
+        client,
+        'getMe',
+        (response) => response.body.results
+       ).then(onSuccess).catch(onError);
+    });
+  });
+
+});
